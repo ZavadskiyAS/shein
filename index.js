86d86703a44569e616f6c496744386940a77855f
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-08-28 23:52:53 +0300

    scroll up

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 1200) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
